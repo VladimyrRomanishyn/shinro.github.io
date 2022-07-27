4b4bf1400504ad14bb01b1c264937b6a20769df3
Vladimir <shinro@ua.fm>
2022-07-27 17:44:35 +0300

    add context specs

--- libs/builder-feature/src/lib/directives/context.directive.spec.ts
+++ libs/builder-feature/src/lib/directives/context.directive.spec.ts
@@ -5,12 +5,32 @@ const elRefStub = {
       localName: 'editor'
     }
   }
+};
+const eventStub = {
+  preventDefault: () => {},
+  target: {
+    localName: 'div',
+    parentElement: {
+      localName: 'editor'
+    }
+  }
 }
 
 describe('ContextDirective', () => {
+  let directive: ContextDirective;
+
+  beforeEach(() => {
+     directive = new ContextDirective(elRefStub);
+  })
 
   it('should create an instance', () => {
-    const directive = new ContextDirective(elRefStub);
     expect(directive).toBeTruthy();
+    expect(directive.localName === elRefStub.nativeElement.parentElement.localName);
+  });
+
+  it( 'should call contextEvent', () => {
+    const spy = jest.spyOn(directive.contextEvent, 'emit');
+    directive.context(eventStub as unknown as PointerEvent);
+    expect(spy).toHaveBeenCalledWith(eventStub);
   });
 });
