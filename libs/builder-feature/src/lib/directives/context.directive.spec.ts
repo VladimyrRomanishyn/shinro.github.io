491a9426f8eec063276ce45733136886ba40a1f9
Vladimir <shinro@ua.fm>
2022-07-27 17:02:56 +0300

    add ng-mocks

--- libs/builder-feature/src/lib/directives/context.directive.spec.ts
+++ libs/builder-feature/src/lib/directives/context.directive.spec.ts
@@ -1,8 +1,16 @@
 import { ContextDirective } from './context.directive';
+const elRefStub = {
+  nativeElement: {
+    parentElement: {
+      localName: 'editor'
+    }
+  }
+}
 
 describe('ContextDirective', () => {
+
   it('should create an instance', () => {
-    const directive = new ContextDirective({});
+    const directive = new ContextDirective(elRefStub);
     expect(directive).toBeTruthy();
   });
 });
