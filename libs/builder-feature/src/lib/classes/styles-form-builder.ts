e52d4176bbfadb0ac4159e1f5e8a8e4b536715f1
Vladimir <shinro@ua.fm>
2023-01-18 17:36:29 +0200

    styles form builder progress

--- libs/builder-feature/src/lib/classes/styles-form-builder.ts
+++ libs/builder-feature/src/lib/classes/styles-form-builder.ts
@@ -1,26 +1,69 @@
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder } from '@angular/forms';
+
 export type CSSProperty =  
 | 'width' | 'height' | 'margin' | 'padding' | 'border' ;  
 
 export type ValueType = 'percentage' | 'pixels' | 'short' | 'full';
 
-export type StylesForm =  {
-    [key in CSSProperty]: {
-        [key in ValueType]: {
-            editable: boolean;
-            value: string | number;
-        }
-    };
+export type FormControlsShape =  {
+    editable: boolean;
+    value: string | number | null;
+};
+
+export type StylesFormConfig = {
+    property: CSSProperty,
+    valueTypes: Array<[ValueType, FormControlsShape]>
 };
 
 export class StylesFormBuilder extends FormBuilder {
     constructor(private node: HTMLElement) {
         super();
     }
-    public  createStylesForm( pattern?: StylesForm) {
-        console.log(this.node, pattern);
-        return this.group({
-            width: 100 })
-              
+
+    public  createStylesForm(config: Array<StylesFormConfig>): FormArray {
+        return this.array(config.map((propConfig) => {
+            return this.array(propConfig.valueTypes.map(
+                ([type, control]) => {
+                    switch(type) {
+                        case 'percentage': 
+                            return this.setPercentage(propConfig.property, type, control);
+                        case 'pixels':
+                            return this.setPixels(propConfig.property, type, control);
+                        case 'short':
+                            return this.setShort(propConfig.property, type, control);
+                        case 'full': 
+                            return this.setFull(propConfig.property, type, control);            
+                    }
+                })
+            )
+        }));
+    }
+
+    private setPercentage(property: CSSProperty,type: ValueType, control: FormControlsShape) {
+        const parent = this.node.parentElement as HTMLElement;
+        const parentWidth = getComputedStyle(parent).getPropertyValue(property).slice(0, -2)
+        const width = getComputedStyle(this.node).getPropertyValue(property).slice(0, -2)
+        control.value = +width / +parentWidth * 100;
+        return this.group(control);
+    }
+
+    private setPixels(property: CSSProperty,type: ValueType, control: FormControlsShape) {
+        const width = getComputedStyle(this.node).getPropertyValue(property);
+        control.value = width;
+        return this.group(control);
+    }
+
+    private setShort(property: CSSProperty,type: ValueType, control: FormControlsShape) {
+        debugger;
+        return this.group(
+            {}
+        )
+    }
+
+    private setFull(property: CSSProperty,type: ValueType, control: FormControlsShape) {
+        debugger;
+        return this.group(
+            {}
+        )
     }
 }
---
+++ libs/builder-feature/src/lib/views/builder-view/style-section/sections-config.ts
@@ -0,0 +1,72 @@
+import { FormControlsShape, StylesFormConfig } from '../../../classes/styles-form-builder';
+
+export enum SectionsEnum {
+    boxModel = 'box-model',
+    grid = 'grid',
+    flex = 'flex',
+}
+
+
+export interface Section {
+    name: string,
+    value: SectionsEnum,
+    stylesFormCofig: Array<StylesFormGroup>
+}
+
+const controlsBlueprint: FormControlsShape = {editable: false, value: null };
+
+const boxModelPage: Array<StylesFormConfig> = [
+    {
+        property: 'width',
+        valueTypes: [
+            ['pixels', {...controlsBlueprint}],
+            ['percentage', {...controlsBlueprint}],
+        ]
+    },
+    {
+        property: 'height',
+        valueTypes: [
+            ['pixels', {...controlsBlueprint}],
+            ['percentage', {...controlsBlueprint}],
+        ]
+    },
+    {
+        property: 'margin',
+        valueTypes: [
+            ['pixels', {...controlsBlueprint}],
+            ['percentage', {...controlsBlueprint}],
+        ]
+    },
+    {
+        property: 'padding',
+        valueTypes: [
+            ['pixels', {...controlsBlueprint}],
+            ['percentage', {...controlsBlueprint}],
+        ]
+    },
+    {
+        property: 'border',
+        valueTypes: [
+            ['short', {...controlsBlueprint}],
+            ['full', {...controlsBlueprint}],
+        ]
+    },
+]
+
+export const sectionsCofig: Section[] = [
+    {
+        name: 'Box Model, Positioning',
+        value: SectionsEnum.boxModel,
+        stylesFormCofig: boxModelPage
+    },
+    {
+        name: 'Grid',
+        value: SectionsEnum.grid,
+        stylesFormCofig: boxModelPage
+    },
+    {
+        name: 'Flex',
+        value: SectionsEnum.flex,
+        stylesFormCofig: boxModelPage
+    },
+]
