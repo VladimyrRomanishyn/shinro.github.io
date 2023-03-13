8da143b6cbb28f1c6ca4bcb0849b07f53df58e55
Vladimir <shinro@ua.fm>
2023-03-13 08:55:07 +0200

    add grid properties

--- libs/builder-feature/src/lib/types/form-types.ts
+++ libs/builder-feature/src/lib/types/form-types.ts
@@ -1,7 +1,8 @@
 
 export enum SectionsEnum {
     common = 'Common',
-    structure = 'Structure',
+    flexbox = 'Flexbox',
+    grid = 'Grid',
 }
 
 
@@ -17,9 +18,11 @@ export type CSSProperty =
     | 'padding-left' | 'padding-top' | 'opacity' | 'zoom' | 'display'
     | 'flex-direction' | 'flex-wrap' | 'flex-flow' | 'justify-content'
     | 'gap' | 'order' | 'flex-grow' | 'flex-shrink' | 'flex-basis'| 'flex'
-    | 'align-self' | 'align-items' | 'align-content' ;
+    | 'align-self' | 'align-items' | 'align-content' | 'grid-template'
+    | 'justify-items' | 'grid-area' | 'justify-self';
 
-export type ValueType = 'percentage' | 'pixels' | 'short' | 'shortWithColorPicker' | 'dropdown';
+export type ValueType = 'percentage' | 'pixels' | 'short' | 'shortWithColorPicker' 
+    | 'dropdown' | 'textarea';
 
 export type FormControlsShape = {
     changed: boolean;
@@ -35,6 +38,7 @@ export type FormControlsShape = {
 
 export type StylesFormConfig = {
     property: CSSProperty,
+    ngStyle?: {[key: string]: string};
     valueTypes: Array<[ValueType, FormControlsShape]>
 };
 
