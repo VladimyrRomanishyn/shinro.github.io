3eb5351ee68a892a2643ea2f7d19e7f19340cbad
Vladimir <shinro@ua.fm>
2023-03-14 08:04:38 +0200

    start export feature dev

--- libs/builder-feature/src/lib/views/builder-view/toolbar/toolbar.component.ts
+++ libs/builder-feature/src/lib/views/builder-view/toolbar/toolbar.component.ts
@@ -1,22 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { takeUntil } from 'rxjs/operators';
+import { ExportGenerator } from '../../../classes/export-generator';
+import { builderFeatureKey, BuilderFeatureState } from '../../../state/builder-feature.reducer';
 
 @Component({
   selector: 'builder-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
-  @Input() set target(v:  HTMLElement | undefined) {
-    this._target = v;
+export class ToolbarComponent  {
+  // public target: HTMLElement | undefined;
+  // private destroy$: EventEmitter<void> = new EventEmitter()
+  
+  // constructor(
+  //   private store: Store<{ [builderFeatureKey]: BuilderFeatureState }>,
+  // ) { }
 
-    if (v) {
-      this.compStyles = getComputedStyle(v);
-    }
-  }
-  get target(): HTMLElement | undefined {
-    return this._target
-  }
+  // ngOnInit(): void {
+  //   this.store.select(state => state[builderFeatureKey].target)
+  //   .pipe(
+  //     takeUntil(this.destroy$)
+  //   )
+  //   .subscribe((target: HTMLElement | undefined) => {
+  //     this.target = target;
+  //   })
+  // }
 
-  private _target: HTMLElement | undefined;
-  compStyles: CSSStyleDeclaration | undefined;
+  // ngOnDestroy(): void {
+  //     this.destroy$.emit();
+  // }
+
+  public export(): void {
+    ExportGenerator.generate(document.querySelector('.editor') as HTMLElement);
+  }
 }
