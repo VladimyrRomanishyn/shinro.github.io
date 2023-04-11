be76425e2ef38beff16fdd72a743348087e5a871
Vladimir <shinro@ua.fm>
2023-04-11 13:24:20 +0300

    add prime modules & tooltiip

--- libs/builder-feature/src/lib/views/builder-view/toolbar/toolbar.component.ts
+++ libs/builder-feature/src/lib/views/builder-view/toolbar/toolbar.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { first } from 'rxjs/operators';
 import { ExportService } from '../../../services/export.service';
 import { builderFeatureKey, BuilderFeatureState } from '../../../state/builder-feature.reducer';
 
 @Component({
   selector: 'builder-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+  styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent  {
   constructor
@@ -15,13 +14,4 @@ export class ToolbarComponent  {
     private store: Store<{ [builderFeatureKey]: BuilderFeatureState }>,
     public exportSvc: ExportService
   ) {}
-
-  public exportImage(): void {
-    this.store.select(state => state[builderFeatureKey].target)
-      .pipe(first())
-      .subscribe((target: HTMLElement | undefined) => {
-        this.exportSvc.exportAsImage(target);
-      })
-    
-  }
 }
